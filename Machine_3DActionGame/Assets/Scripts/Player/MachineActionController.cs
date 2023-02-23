8359f0194124a0f24f4bc8bbc315bad32d45ee73
aki3 <hiro13424@gmail.com>
2023-02-23 13:36:18 +0900

    chore : 不要な移動処理の削除

--- Machine_3DActionGame/Assets/Scripts/Player/MachineActionController.cs
+++ Machine_3DActionGame/Assets/Scripts/Player/MachineActionController.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class MachineActionController : MonoBehaviour
 {
     private MoveMachine moveMachine;
     private CharacterController characterController;
-    [SerializeField]
-    private PlayerInput inputMap;
     [SerializeField]
     private Camera camera;
 
@@ -16,27 +13,9 @@ public class MachineActionController : MonoBehaviour
         moveMachine = new MoveMachine();
     }
 
-    //private void OnEnable()
-    //{
-    //    inputMap.actions["Movement"].performed += OnMove;
-    //}
-
-    //private void OnDisable()
-    //{
-    //    inputMap.actions["Movement"].performed -= OnMove;
-    //}
-
     private void Update()
     {
         moveMachine.MovementCalculation(characterController, camera, this.transform);
     }
-
-    //public void OnMove(InputAction.CallbackContext context)
-    //{
-    //    var value = context.ReadValue<Vector2>();
-    //    var direction = new Vector3(value.x, 0, value.y);
-    //    moveMachine.MovementCalculation(characterController, camera, this.transform, direction);
-    //    Debug.Log("‚“‚“‚“"+value);
-    //}
 
 }
