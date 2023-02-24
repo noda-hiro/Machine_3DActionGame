f91c42cab870fe37e9b826788b86ddf2503b328c
aki3 <hiro13424@gmail.com>
2023-02-24 23:57:32 +0900

    feat : ブースト基底クラスをコントローラーに追加

--- Machine_3DActionGame/Assets/Scripts/Player/MachineActionController.cs
+++ Machine_3DActionGame/Assets/Scripts/Player/MachineActionController.cs
@@ -1,21 +1,125 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MachineActionController : MonoBehaviour
 {
-    private MoveMachine moveMachine;
+    private MoveMachineKit moveMachine;
+    private BoostMachineKit boostMachine;
     private CharacterController characterController;
+    private Coroutine boostValueRecoveryCoroutine;
+    private Coroutine boostValueConsumptionCoroutine;
+
+    [SerializeField]
+    private BoostParameter boostParameter;
     [SerializeField]
     private Camera camera;
+    private InputAction inputAction;
+    [SerializeField]
+    private PlayerInput PlayerInput;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        moveMachine = new MoveMachine();
+        inputAction = new InputAction();
+        moveMachine = new MoveMachineKit();
+        boostMachine = new BoostMachineKit(boostParameter.maxBoostValue, boostParameter.consumptionBoostValue, boostParameter.recoveryTimeValue, boostParameter.recoveryValue,boostParameter.consumptionTimeValue);
+        SetUpInputMap();
     }
 
     private void Update()
     {
-        moveMachine.MovementCalculation(characterController, camera, this.transform);
+        moveMachine.MovementCalculation(characterController, camera, transform);
+        Debug.Log(boostMachine.GetCurrentBoostValue());
     }
+
+    #region ブースト関連
+
+    private IEnumerator BoostConsumption()
+    {
+        while (boostMachine.CanBoostConsumption())
+        {
+            boostMachine.ConsumptionBoost();
+            yield return new WaitForSeconds(boostMachine.GetConsumptionTime());
+        }
+        StopBoosConsumption();
+    }
+
+    private IEnumerator BoostRecovery()
+    {
+        while (boostMachine.CanBoostRecovery())
+        {
+            boostMachine.ConsumptionBoostValueRecovery();
+            yield return new WaitForSeconds(boostMachine.GetRecoveryTime());
+        }
+        StopBoostRecovery();
+    }
+    #endregion
+
+    #region コルーチン関連
+    private void StartBoostRecovery()
+    {
+        if (boostValueRecoveryCoroutine != null) return;
+
+        boostValueRecoveryCoroutine = StartCoroutine(BoostRecovery());
+    }
+
+    private void StopBoostRecovery()
+    {
+        if (boostValueRecoveryCoroutine == null) return;
+
+        StopCoroutine(boostValueRecoveryCoroutine);
+        boostValueRecoveryCoroutine = null;
+    }
+
+    private void StartBoostConsumption()
+    {
+        if (boostValueConsumptionCoroutine != null) return;
+
+        moveMachine.GetIsBoost(true);
+        boostValueConsumptionCoroutine = StartCoroutine(BoostConsumption());
+    }
+    private void StopBoosConsumption()
+    {
+        if (boostValueConsumptionCoroutine == null) return;
+
+        moveMachine.GetIsBoost(false);
+        StopCoroutine(boostValueConsumptionCoroutine);
+        boostValueConsumptionCoroutine = null;
+    }
+
+    #endregion
+
+    #region ボタン処理
+    private void SetUpInputMap()
+    {
+        inputAction = new InputAction();
+        inputAction = PlayerInput.currentActionMap[boostParameter.boostName];
+    }
+
+    private void OnEnable()
+    {
+        inputAction.performed += OnStarted;
+        inputAction.canceled += OnCanceled;
+    }
+
+    private void OnDisable()
+    {
+        inputAction.performed -= OnStarted;
+        inputAction.canceled -= OnCanceled;
+    }
+
+    public void OnStarted(InputAction.CallbackContext context)
+    {
+        StopBoostRecovery();
+        StartBoostConsumption();
+    }
+
+    public void OnCanceled(InputAction.CallbackContext context)
+    {
+        StopBoosConsumption();
+        StartBoostRecovery();
+    }
+    #endregion
 
 }
